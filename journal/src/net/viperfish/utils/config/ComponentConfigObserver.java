0af3fb258ef74897b2496a140d8443aba78d7541
sdai <sdai@viperfish.net>
2015-12-06 19:39:27 -0500

    re-added the observer

---
+++ journal/src/net/viperfish/utils/config/ComponentConfigObserver.java
@@ -0,0 +1,5 @@
+package net.viperfish.utils.config;
+
+public interface ComponentConfigObserver {
+	public void sendNotify(ComponentConfig c);
+}
--- journal/src/net/viperfish/utils/config/Configuration.java
+++ journal/src/net/viperfish/utils/config/Configuration.java
@@ -8,6 +8,12 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/**
+ * the configuration utility
+ * 
+ * @author sdai
+ *
+ */
 public class Configuration {
 
 	private static Map<String, ComponentConfig> data;
@@ -21,15 +27,38 @@ public class Configuration {
 		}
 	}
 
+	/**
+	 * get a configuration unit with a id of key
+	 * 
+	 * @param key
+	 *            the identifier of the unit
+	 * @return the configuration unit
+	 */
 	public static ComponentConfig get(String key) {
 		return data.get(key);
 	}
 
+	/**
+	 * add a config unit to the this manager
+	 * 
+	 * @param key
+	 *            the identifier of the configurationUnit
+	 * @param value
+	 *            the unit
+	 * @return the added unit
+	 */
 	public static ComponentConfig put(String key, ComponentConfig value) {
 		value.fillInDefault();
 		return data.put(key, value);
 	}
 
+	/**
+	 * store all configuration to xml files located in configuration directory,
+	 * currently ./config
+	 * 
+	 * @throws IOException
+	 *             if any thing went wrong when storing
+	 */
 	public static void persistAll() throws IOException {
 		for (Entry<String, ComponentConfig> i : data.entrySet()) {
 			i.getValue().setProperty("ConfigFileLocation",
@@ -43,6 +72,13 @@ public class Configuration {
 		}
 	}
 
+	/**
+	 * load all added configuration from their config files with the
+	 * corresponding names located in the ./config directory
+	 * 
+	 * @throws IOException
+	 *             if error on reading, and parsing
+	 */
 	public static void loadAll() throws IOException {
 		for (Entry<String, ComponentConfig> i : data.entrySet()) {
 			i.getValue().setProperty("ConfigFileLocation",
@@ -55,6 +91,11 @@ public class Configuration {
 		}
 	}
 
+	/**
+	 * get all added component
+	 * 
+	 * @return all component
+	 */
 	public static Iterable<ComponentConfig> allComponent() {
 		Set<ComponentConfig> all = new HashSet<>();
 		for (Entry<String, ComponentConfig> i : data.entrySet()) {
@@ -63,6 +104,11 @@ public class Configuration {
 		return all;
 	}
 
+	/**
+	 * call the fillInDefault method on all ComponentConfig units
+	 * 
+	 * @see ComponentConfig#fillInDefault()
+	 */
 	public static void defaultAll() {
 		for (Entry<String, ComponentConfig> i : data.entrySet()) {
 			i.getValue().fillInDefault();
