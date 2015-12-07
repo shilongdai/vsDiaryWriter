7dd683f2f8609feb7e7eaf50f27e97e094036d70
sdai <sdai@viperfish.net>
2015-12-06 22:31:40 -0500

    updated pom to support migLayout, and remove binary

--- journal/src/net/viperfish/journal/gui/setup/FirstTimeSetup.java
+++ journal/src/net/viperfish/journal/gui/setup/FirstTimeSetup.java
@@ -1,18 +1,19 @@
 package net.viperfish.journal.gui.setup;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
 
 public class FirstTimeSetup extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5531765124830505855L;
 	private JPanel contentPane;
-
 
 	/**
 	 * Create the frame.
@@ -23,30 +24,31 @@ public class FirstTimeSetup extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("inset 0, fill", "[grow]", "[grow][]"));
-		
-		
+		contentPane.setLayout(new MigLayout("inset 0, fill", "[grow]",
+				"[grow][]"));
+
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, "cell 0 1,alignx right,growy");
-		panel_1.setLayout(new MigLayout("inset 0, fill", "[55px][55px]", "[23px]"));
-		
+		panel_1.setLayout(new MigLayout("inset 0, fill", "[55px][55px]",
+				"[23px]"));
+
 		JButton btnBack = new JButton("Back");
 		btnBack.setEnabled(false);
 		panel_1.add(btnBack, "cell 0 0,alignx right,aligny top");
-		
+
 		JButton btnNext = new JButton("Next");
 		btnNext.setEnabled(false);
 		panel_1.add(btnNext, "cell 1 0,alignx right,aligny top");
 		loadConfiguration();
 	}
-	
+
 	public ConfigView view;
-	
-	public void loadConfiguration(){
+
+	public void loadConfiguration() {
 		setView(new SimpleOrExpert());
 	}
-	
-	public void setView(ConfigView view){
+
+	public void setView(ConfigView view) {
 		contentPane.add(view, "cell 0 0,grow");
 	}
 
