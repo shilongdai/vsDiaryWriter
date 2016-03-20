122b2d2214b690cb60d9a5f853b2ed68222511d1
sdai <sdai@viperfish.net>
2016-03-20 11:02:54 -0400

    updates window version

--- journal/src/net/viperfish/journal/swtGui/JournalWindow.java
+++ journal/src/net/viperfish/journal/swtGui/JournalWindow.java
@@ -210,7 +210,7 @@ public class JournalWindow {
 		display = Display.getDefault();
 		shell = new Shell();
 		shell.setSize(495, 480);
-		shell.setText("vsDiary - 1.1.0");
+		shell.setText("vsDiary - 1.2.1");
 		shell.setLayout(new GridLayout(13, false));
 
 		errorReporter = new ExceptionDisplayer(shell);
