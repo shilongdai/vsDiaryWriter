3e52939e6ecc4ee8218acc5b3832800c3fee4704
Shilong Dai <sdai@viperfish.net>
2017-02-10 20:03:36 -0500

    updates build script.

--- build.sh
+++ build.sh
@@ -10,17 +10,16 @@ cp -rv ../journal journalMac64;
 
 echo "copying additional files";
 for i in $(ls); do
-	cp -rv ../editor $i -r;
-	cp -rv ../i18n $i -r;
+	cp -rv ../editor $i;
+	cp -rv ../i18n $i;
+	cp -rv ../config $i;
+	cp -rv ../data.mv.db $i;
 done;
 echo "configuring build dependencies";
 sed -i -e 's/org.eclipse.swt.gtk.linux.x86_64/org.eclipse.swt.win32.win32.x86/g' journalWindows32/pom.xml;
 sed -i -e 's/org.eclipse.swt.gtk.linux.x86_64/org.eclipse.swt.gtk.linux.x86/g' journalLinux32/pom.xml;
 sed -i -e 's/org.eclipse.swt.gtk.linux.x86_64/org.eclipse.swt.cocoa.macosx.x86_64/g' journalMac64/pom.xml;
 
-xulRunner[0]="xulrunner-24.0.en-US.linux-i686.tar.bz2";
-xulRunner[1]="xulrunner-24.0.en-US.linux-x86_64.tar.bz2";
-xulRunner[2]="xulrunner-24.0.en-US.mac.tar.bz2";
 xulRunner[3]="xulrunner-24.0.en-US.win32.zip";
 mozilla="https://ftp.mozilla.org/pub/xulrunner/releases/24.0/runtimes/"
 
@@ -39,30 +38,29 @@ for i in $(ls); do
 	mv ../i18n .;
 	cp ../../../LICENSE .
 
+	mv ../data.mv.db .;
+	mv ../config .;
+
 	#get XULRunner
 	if [ $i = "journalWindows32" ]; then
 		wget $mozilla${xulRunner[3]};
 		unzip ${xulRunner[3]};
-		zip -r $i.zip full-journal*.jar editor xulrunner $(ls | grep launcher.sh) LICENSE i18n;
+		zip -r $i.zip full-journal*.jar editor xulrunner $(ls | grep launcher.sh) LICENSE i18n config data.mv.db;
 	fi
 
 	if [ $i = "journalLinux32" ]; then
-		wget $mozilla${xulRunner[0]};
-		tar -jxvf ${xulRunner[0]};
 		cp ../../../linux-launcher.sh .
-		tar -cJvf $i.tar.xz full-journal*.jar editor xulrunner $(ls | grep launcher.sh) LICENSE i18n;
+		tar -cJvf $i.tar.xz full-journal*.jar editor $(ls | grep launcher.sh) LICENSE i18n config data.mv.db;
 	fi
 
 	if [ $i = "journalLinux64" ]; then
-		wget $mozilla${xulRunner[1]};
-		tar -jxvf ${xulRunner[1]};
 		cp ../../../linux-launcher.sh .
-		tar -cJvf $i.tar.xz full-journal*.jar editor xulrunner $(ls | grep launcher.sh) LICENSE i18n;
+		tar -cJvf $i.tar.xz full-journal*.jar editor $(ls | grep launcher.sh) LICENSE i18n config data.mv.db;
 	fi
 
 	if [ $i = "journalMac64" ]; then
 		cp ../../../mac-launcher.sh .
-		tar -cJvf $i.tar.xz full-journal*.jar editor $(ls | grep launcher.sh) LICENSE i18n;
+		tar -cJvf $i.tar.xz full-journal*.jar editor $(ls | grep launcher.sh) LICENSE i18n config data.mv.db;
 	fi
 	
 	echo "copying product to $1";
